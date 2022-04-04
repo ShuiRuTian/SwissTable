d9d011f600f7f9cd8bbd8efce8fd8e1a413893e8
songgao <songgao@microsoft.com>
2022-04-04 13:08:24 +0800

    add benchmark proj

---
+++ Benchmark/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
+using System.Collections.Generic;
+
+namespace Benchmark
+{
+    // As long as the instance is readonly, it could be inlined.
+    [DisassemblyDiagnoser(maxDepth: 3, printSource: false, printInstructionAddresses: false)]
+    public class GroupAndBitMask
+    {
+        byte[] data;
+        [GlobalSetup]
+        public void InitializeContainsValue()
+        {
+            data = new byte[100];
+        }
+
+        [Benchmark]
+        public unsafe IBitMask AggressiveLineForRuntime()
+        {
+            fixed (byte* ptr = &data[0])
+            {
+                IBitMask bitMask = new Sse2Group().load(ptr)
+                    .match_full()
+                    .remove_lowest_bit()
+                    .remove_lowest_bit()
+                    .remove_lowest_bit();
+                return bitMask;
+            }
+        }
+    }
+}
---
+++ Benchmark/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
+
+namespace Benchmark
+{
+    // As long as the instance is readonly, it could be inlined.
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkSwitcher
+                .FromAssembly(typeof(Program).Assembly)
+                .Run(args);
+        }
+    }
+}
